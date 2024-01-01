226949c4899925414afefd6d85d86a1c37127c07
Nedahost <aggelos13227@gmail.com>
2024-01-01 08:53:40 +0200

    Update services.php
    new

--- inc/services.php
+++ inc/services.php
@@ -53,3 +53,51 @@ function nedahost_services(){
 	);
     
 }
+
+function custom_section_fields() {
+  add_meta_box(
+      'custom_section_fields',
+      'Πρόσθετα Πεδία',
+      'custom_section_fields_callback',
+      'nedahost_services',
+      'normal',
+      'high'
+  );
+}
+add_action('add_meta_boxes', 'custom_section_fields');
+
+function custom_section_fields_callback($post) {
+  $extra_field_1_value = get_post_meta($post->ID, 'extra_field_1', true);
+  $extra_field_2_value = get_post_meta($post->ID, 'extra_field_2', true);
+  $extra_field_3_value = get_post_meta($post->ID, 'extra_field_3', true);
+
+  echo '<label for="extra_field_1">Πρόσθετο Πεδίο 1:</label>';
+  echo '<input type="text" id="extra_field_1" name="extra_field_1" value="' . esc_attr($extra_field_1_value) . '"><br>';
+
+  echo '<label for="extra_field_1_link">Σύνδεσμος Πεδίου 1:</label>';
+  echo '<input type="url" id="extra_field_1_link" name="extra_field_1_link" value="' . esc_attr(get_post_meta($post->ID, 'extra_field_1_link', true)) . '"><br>';
+
+
+  echo '<label for="extra_field_2">Πρόσθετο Πεδίο 2:</label>';
+  echo '<input type="text" id="extra_field_2" name="extra_field_2" value="' . esc_attr($extra_field_2_value) . '"><br>';
+
+  echo '<label for="extra_field_3">Πρόσθετο Πεδίο 3:</label>';
+  echo '<input type="text" id="extra_field_3" name="extra_field_3" value="' . esc_attr($extra_field_3_value) . '">';
+}
+
+function save_custom_section_fields($post_id) {
+  if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
+
+  if (isset($_POST['extra_field_1'])) {
+      update_post_meta($post_id, 'extra_field_1', sanitize_text_field($_POST['extra_field_1']));
+  }
+
+  if (isset($_POST['extra_field_2'])) {
+      update_post_meta($post_id, 'extra_field_2', sanitize_text_field($_POST['extra_field_2']));
+  }
+
+  if (isset($_POST['extra_field_3'])) {
+      update_post_meta($post_id, 'extra_field_3', sanitize_text_field($_POST['extra_field_3']));
+  }
+}
+add_action('save_post', 'save_custom_section_fields');

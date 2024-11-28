e2b417dc1db887c3fe71b4ff018f1f2476df9f12
Nedahost <aggelos13227@gmail.com>
2024-11-28 17:14:09 +0200

    main

--- footer.php
+++ footer.php
@@ -1,83 +1,8 @@
     </main>
     <footer>
-        <div class="wrapper"><!-- wrapper start -->
-            <div class="outerfooter"><!-- outerfooter start -->
-                <div class="footerlogo"><!-- footer logo start -->
-                    <a href="">
-                        <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/logo.png" />
-                    </a>
-                </div><!-- footer logo end -->
-                <div class="footerdetails"><!-- footer details start -->
-                    <div class="footerdetails__list"><!-- list start -->
-                        <h5>Επικοινωνία:</h5>
-                        <ul>
-                            <li>T. +30 697 5686 473</li>
-                            <li>Δ. ΕΛΑΣΣΩΝΟΣ 16, ΠΕΡΙΣΤΕΡΙ ΑΤΤΙΚΗΣ, Τ.Κ. 121 37</li>
-                            <li>E. INFO@NEDAHOST.GR</li>
-                        </ul>
-                        <div class="socialmedia"><!-- social media start -->
-                            <h6>Ακολούθησε μας στα social media:</h6>
-                            <ul>
-                                <li>
-                                    <a href="https://www.linkedin.com/company/18602896" target="_blank">
-                                        <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/linkedin.png" alt="Linkedin" />
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="https://www.facebook.com/nedahost" target="_blank">
-                                        <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/facebook.png" alt="Facebook" />
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="https://www.instagram.com/nedahost/" target="_blank">
-                                        <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/instagram.png" alt="Instagram" />
-                                    </a>
-                                </li>
-                            </ul>
-                        </div><!-- social media end-->
-                    </div><!-- list end -->
-                    <div class="footerdetails__map"><!-- map start -->
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2071.561297849435!2d23.668057423909232!3d38.02365704619108!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14a1a3974ed9527b%3A0xa23668de2665e4b3!2zTmVkYWhvc3QgLSDOms6xz4TOsc-DzrrOtc-Fzq4gzpnPg8-Ezr_Pg861zrvOr860z4nOvQ!5e0!3m2!1sel!2sgr!4v1652691495554!5m2!1sel!2sgr" width="300" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                    </div><!-- map end -->
-                </div><!-- footer details end -->
-            </div><!-- outerfooter end -->
-        </div><!-- wrapper end -->
-    </footer>
-
-    <script>
-  jQuery(document).ready(function($) {
-            var forEach = function (t, o, r) {
-                if ("[object Object]" === Object.prototype.toString.call(t))
-                    for (var c in t) Object.prototype.hasOwnProperty.call(t, c) && o.call(r, t[c], c, t);
-                else for (var e = 0, l = t.length; l > e; e++) o.call(r, t[e], e, t);
-            };
         
-            var hamburgers = document.querySelectorAll(".hamburger");
-            if (hamburgers.length > 0) {
-                forEach(hamburgers, function (hamburger) {
-                    hamburger.addEventListener("click", function () {
-                        this.classList.toggle("is-active");
-                        document.body.classList.toggle("menu-open");
-                    }, false);
-                });
-            }
-        });
-        </script>
-        <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            var accordionTitles = document.getElementsByClassName('accordion-title');
-            for (var i = 0; i < accordionTitles.length; i++) {
-                accordionTitles[i].addEventListener('click', function() {
-                    var questionText = this.querySelector('.title-text').textContent;
-                    gtag('event', 'faq_accordion_click', {
-                        'question_text': questionText
-                    });
-                });
-            }
-        });
-</script>
-
-     <?php wp_footer(); ?>
+    </footer>
+    <?php wp_footer(); ?>
 
     </body>
 </html>
--- header.php
+++ header.php
@@ -13,63 +13,14 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
        
-        <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@300;400&family=Roboto+Condensed:wght@400;700&display=swap" rel="stylesheet"> 
+        <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap" rel="stylesheet">
         <?php wp_head(); ?>
         
     </head>
     <body <?php body_class(); ?>>
         <header>
-            <div class="topheader"><!-- flex topheade start -->
-                <figure>
-                    <?php
-                    $header_image = get_header_image();
-                    if ( ! empty( $header_image ) ) { ?>
-                    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" rel="home">
-                        <img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="<?php echo get_bloginfo( 'name' ); ?>" />
-                    </a>
-                    <?php } ?>
-                </figure>
-                <div id="outermenu">
-
-                <div class="hamburger hamburger--slider-r">
-    <div class="hamburger-box">
-      <div class="hamburger-inner"></div>
-    </div>
-  </div>
-  <div id="navigation">
-    <nav>
-  <?php
-                    wp_nav_menu(
-                        array(
-                            'theme_location' => 'primary',
-                            'menu_class' => 'dropdown'
-                        )
-                    );
-                    ?>
-</nav>
-  </div>
-                </div>
-            </div><!-- flex topheade end -->
-            <div class="sectiontitle"><!-- section title start -->
-            <?php if(is_home()){ ?>
-                <h1 class="headtitle">NEDAHOST</h1>
-                <span>a digital agency for web solutions</span>
-            <?php }elseif(is_page()){ ?>
-                <h1><?php echo esc_html( get_the_title() ); ?></h1>
-                <p>
-                  
-                </p>
-                <?php } ?>
-                
-            </div><!-- section title end -->
-            <?php if(is_home()){ ?>
-            <div class="readmore"><!-- read more start -->
-                <div class="read">
-                    <a href="" >
-                        Ας φτιάξουμε <br />κάτι μοναδικό.
-                    </a>
-                </div>
-            </div><!-- read more end -->
-            <?php } ?>
+            <div class="text">
+                A web design and development agency in Athens
+            </div>
         </header>
         <main>
